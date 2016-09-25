8b97e1e7ebbe6f6893aef9742ebfa4a7a3d81a18
Jay Young <jayayoung@gmail.com>
2016-09-25 20:10:35 +1000

    Upload Files

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/*
+Use jQuery to turn the light on and off.
+
+You will need to change the bulb image to reflect its state as well as the background color.
+
+You will also need to update the text on the button.
+*/
+$(function(){
+
+
+
+
+
+
+});
